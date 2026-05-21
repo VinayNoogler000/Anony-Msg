9c532f3c0ae921898c0815b957c2c0fcf21cfc89
VinayNoogler000 <vinaytambey000@gmail.com>
2026-05-21 22:46:04 +0530

    Refactor DB connection status handling to use a simple variable instead of an object whos type is declared in the same file. This simplifies the code and reduces unnecessary complexity.

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,20 +1,16 @@
 import mongoose from "mongoose";
 
-type ConnectionObject = {
-    isConnected?: number,
-}
-
-const connection: ConnectionObject = {}
+let connectionStatus: number | undefined;
 
 async function dbConnect(): Promise<void> {
-    if (connection.isConnected) { // if Application is already connected to DB, then exit
+    if (connectionStatus === 1) { // if Application is already connected to DB, then exit
         console.log("Already Connected to DB");
         return;
     }
     
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI || "")
-        connection.isConnected = db.connections[0].readyState;
+        const db = await mongoose.connect(process.env.MONGODB_URI || "");
+        connectionStatus = db.connections[0].readyState;
         console.log("DB Connected Successfully");
     } catch (err) {
         console.log("Database Connection failed: ", err);
