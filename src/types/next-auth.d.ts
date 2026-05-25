7e1b2aa50f235750f0189f6b2f52ac56a2ccd09b
VinayNoogler000 <vinaytambey000@gmail.com>
2026-05-25 18:23:57 +0530

    Enhance Next-Auth types by adding Session and JWT interfaces to include user properties for improved session management

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,4 +1,5 @@
 import "next-auth";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
     interface User {
@@ -7,4 +8,25 @@ declare module "next-auth" {
         isAcceptingMsg?: boolean,
         username?: string
     }
+
+    interface Session {
+        user: {
+            _id?: string,
+            isVerified?: boolean,
+            isAcceptingMsg?: boolean,
+            username?: string 
+        } & DefaultSession['user'] // This is to ensure that by default the session will have the "user" property with the default properties of the session user, so that we can use it properly, without getting any errors. 
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        user: {
+            _id?: string,
+            isVerified?: boolean,
+            isAcceptingMsg?: boolean,
+            username?: string
+        }
+    } 
+
 }
