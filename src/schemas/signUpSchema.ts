41e2ebe0e96be766bdd768090861cfec57abf55e
VinayNoogler000 <vinaytambey000@gmail.com>
2026-07-04 12:30:41 +0530

    Add upper limit to password characters in sign-up schema

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -4,5 +4,5 @@ import { userNameValidation } from "./usernameSchema";
 export const signUpSchema = z.object({
     username: userNameValidation,
     email: z.email("Invalid email address"),
-    password: z.string().min(8, "Password must be atleast 8 characters"),
+    password: z.string().min(8, "Password must be atleast 8 characters").max(16, "Password shouldn't be less than 16 characters")
 });
