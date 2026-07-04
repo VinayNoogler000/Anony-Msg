a4d4770da308eb7ed460b16c59187a0260c99cd4
VinayNoogler000 <vinaytambey000@gmail.com>
2026-07-04 19:20:03 +0530

    Refactor the regex value in usernameValidation Zod Schema to correctly prevent special characters except underscores

--- src/schemas/usernameSchema.ts
+++ src/schemas/usernameSchema.ts
@@ -4,7 +4,7 @@ export const userNameValidation = z
     .string()
     .min(2, {error: "Username must be atleast 2 characters"})
     .max(20, {error: "Username must be lesser than 20 characters"})
-    .regex(/[a-zA-Z][a-zA-Z0-9-_]{3,32}/gi, "Username must not contain special chracters");
+    .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special chracters, except underscore '_'");
 
 export const UsernameQuerySchema = z.object({
     username: userNameValidation
