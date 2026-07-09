8275f0f06bc250a1107892f252adecd5ec7ba388
VinayNoogler000 <vinaytambey000@gmail.com>
2026-07-09 16:17:40 +0530

    Add error messages for invalid fields in sign-in schema validation for better user feedback

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
 export const signInSchema = z.object({
-    identifier: z.string(), // email will be identifier which will be used to search the user in DB
-    password: z.string().min(8, "Password must be atleast 8 characters"),
+    identifier: z.string("Email or Username is Required"), // email will be identifier which will be used to search the user in DB
+    password: z.string("Password is Required").min(8, "Password must be atleast 8 characters"),
 });
