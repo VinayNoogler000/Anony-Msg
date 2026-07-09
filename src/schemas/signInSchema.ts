9144985d97a4b4e52fc01d58df7203ec5169b8a1
VinayNoogler000 <vinaytambey000@gmail.com>
2026-07-09 16:23:13 +0530

    Refactor identifier validation to require minimum 2 characters

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
 export const signInSchema = z.object({
-    identifier: z.string("Email or Username is Required"), // email will be identifier which will be used to search the user in DB
+    identifier: z.string("Email or Username is Required").min(2, "Identifier must be atleast 2 characters"), // email will be identifier which will be used to search the user in DB
     password: z.string("Password is Required").min(8, "Password must be atleast 8 characters"),
 });
