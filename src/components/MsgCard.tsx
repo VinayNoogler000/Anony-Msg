0ee47e839cc63bef21a54e356085eea00724a861
VinayNoogler000 <vinaytambey000@gmail.com>
2026-07-12 18:37:07 +0530

    Add Message Card component with delete functionality, using Card and AlertDialog components of ShadCN

---
+++ src/components/MsgCard.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from './ui/button'
+import { Message } from '@/model/User'
+import { toast } from 'sonner'
+import axios from 'axios'
+import ApiResponse from '@/types/ApiResponse'
+
+type MsgCardProps = { 
+  message: Message,
+  onMessageDelete: (msgId:string) => void 
+}
+
+function MsgCard({ message, onMessageDelete }:MsgCardProps) {
+  const handleDeleteConfirm = async () => {
+    const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`);
+    toast.success(response.data.message, {dismissible: true});
+    onMessageDelete(message._id.toString());
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="destructive" className='w-5 h-5'>X</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your account
+                from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+        <CardDescription>Card Description</CardDescription>
+        <CardAction>Card Action</CardAction>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default MsgCard
