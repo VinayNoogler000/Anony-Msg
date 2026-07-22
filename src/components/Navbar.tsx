ae4916111db4efb3c7fea7b4dc85f08bf08dfb49
VinayNoogler000 <vinaytambey000@gmail.com>
2026-07-22 12:59:51 +0530

    Update Navbar styling and button colors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,19 +9,19 @@ function Navbar() {
   const user = session?.user as User;
 
   return (
-    <nav>
-      <div>
+    <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <a href="#" className="text-xl font-bold mb-4 md:mb-0">AnonyMsg</a>
         { status === "authenticated" ? 
           ( 
             <>
               <span className="mr-4">Welcome, {user?.username || user?.email}! </span> 
-              <Button className="w-full md:w-auto" onClick={() => signOut()}> Logout </Button> 
+              <Button className="w-full md:w-auto bg-slate-100 text-black" onClick={() => signOut()}> Logout </Button> 
             </>
           ) : 
           ( 
             <Link href={"/sign-in"}>
-              <Button className="w-full md:w-auto">Login</Button>
+              <Button className="w-full md:w-auto bg-slate-100 text-black" variant="outline">Login</Button>
             </Link> 
           )
         }
