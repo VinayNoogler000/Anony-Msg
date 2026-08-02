9ab3039b116f25d8047442ebe2ff397445728488
VinayNoogler000 <vinaytambey000@gmail.com>
2026-08-02 15:06:31 +0530

    Moved home-page and the layout consisting of "navbar", from `(app)` folder to `app` folde, which is root of the project.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import AuthProvider from "@/context/AuthProvider";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({subsets:['latin']});
 
@@ -20,8 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
-          {children}  
-          <Toaster />
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            {children}
+            <Toaster />
+          </div>
         </AuthProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useRef } from "react";
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import {
   Carousel,
   CarouselContent,
