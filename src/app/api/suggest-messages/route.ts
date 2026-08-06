e913216141bb33ad242f13605829d875c9e9fee7
VinayNoogler000 <vinaytambey000@gmail.com>
2026-08-06 23:36:33 +0530

    Tested proper working of generating engaging questions by using AI-SDK and Google's Gemini model

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,7 +1,7 @@
 import { streamText, createUIMessageStreamResponse, toUIMessageStream, APICallError } from 'ai';
-import { google } from "@ai-sdk/google";
+import { google } from '@ai-sdk/google'
 
-export async function GET(req: Request): Promise<Response> {
+export async function POST(req: Request): Promise<Response> {
   try {
         const prompt =  "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."; 
 
@@ -11,11 +11,11 @@ export async function GET(req: Request): Promise<Response> {
             onError(error) {
                 console.error("Error while streaming Text from AI-Model: ", error);
             },
-            maxOutputTokens: 400,
+            // maxOutputTokens: 400,
         });
     
         return createUIMessageStreamResponse({
-        stream: toUIMessageStream({ stream: result.stream}),
+            stream: toUIMessageStream({ stream: result.stream}),
         });
     } 
     catch (error) {
