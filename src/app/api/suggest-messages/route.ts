2962ef9656778d02431acab646ba664d86b32114
VinayNoogler000 <vinaytambey000@gmail.com>
2026-08-07 17:21:40 +0530

    - Remove unnecessary try-catch block - Ensured proper logging of streaming errors via `onError` callback - Add model fallback configuration for OpenRouter

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,40 +1,38 @@
 import { streamText, APICallError, createTextStreamResponse, toTextStream, RetryError } from 'ai';
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
 
-export async function POST(req: Request): Promise<Response> {
-  try {
-        const prompt =  "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."; 
+export async function POST() {
+    const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
-        const openrouter = createOpenRouter({
-            apiKey: process.env.AI_API_KEY,
-        });
+    const openrouter = createOpenRouter({
+        apiKey: process.env.AI_API_KEY,
+    });
 
-        const result = streamText({
-            model: openrouter("google/gemma-4-31b-it:free"),
-            prompt,
-            onError(error) {
-                console.error("Error while streaming Text from AI-Model");
+    const result = streamText({
+        model: openrouter("openai/gpt-oss-20b:free"),
+        prompt,
+        onError(error) {
+            const err = error.error as Error;
+            console.log("Streaming Error [api/suggest-messages/route.ts]: ", `${err.name} ---- ${err.cause} ---- ${err.message}`);
+        },
+        maxOutputTokens: 300,
+        providerOptions: { // OpenRouter fallback configuration
+            openrouter: {
+                extraBody: {
+                    models: [
+                        'google/gemma-4-31b-it:free',
+                        'google/gemma-4-26b-a4b-it:free',
+                        'nvidia/nemotron-3.5-content-safety:free',
+                        'nvidia/nemotron-3-ultra-550b-a55b:free',
+                        'inclusionai/ling-3.0-tiny:free'
+                    ],
+                },
             },
-            // maxOutputTokens: 400,
-        });
-    
-        return createTextStreamResponse({
-            stream: toTextStream({ stream: result.stream}),
-        });
-    } 
-    catch (error) {
-        if (error instanceof RetryError) {
-            console.error("High Server Load. Please try again later!");
-            return Response.json({ 
-                success: false,
-                message: "High Server Load. Please try again later!",
-            }, {status: 500});
-        }
-        
-        console.error("Server-side problem. Please try again later! ", error);
-        return Response.json({ 
-            success: false,
-            message: "Server-side problem. Please try again later!"
-        }, {status: 500});
-    }
+        },
+    });
+
+    return createTextStreamResponse({
+        status: 200,
+        stream: toTextStream({ stream: result.stream }),
+    });
 }
