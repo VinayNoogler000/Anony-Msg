ac1cdbbaf1941045a0b19cc7057bae25d58264b8
VinayNoogler000 <vinaytambey000@gmail.com>
2026-08-07 21:52:50 +0530

    Enhance spacing and login/logout button sizing in Navbar component for better UI/UX

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,18 +10,18 @@ function Navbar() {
 
   return (
     <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
         <a href="#" className="text-xl font-bold mb-4 md:mb-0">AnonyMsg</a>
         { status === "authenticated" ? 
           ( 
             <>
               <span className="mr-4">Welcome, {user?.username || user?.email}! </span> 
-              <Button className="w-full md:w-auto bg-slate-100 text-black" onClick={() => signOut()}> Logout </Button> 
+              <Button className="w-fit md:w-auto bg-slate-100 text-black" onClick={() => signOut()}> Logout </Button> 
             </>
           ) : 
           ( 
             <Link href={"/sign-in"}>
-              <Button className="w-full md:w-auto bg-slate-100 text-black" variant="outline">Login</Button>
+              <Button className="w-fit md:w-auto bg-slate-100 text-black" variant="outline">Login</Button>
             </Link> 
           )
         }
