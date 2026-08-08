1a46cc58039cb1821f1c79551b0709228542c32a
VinayNoogler000 <vinaytambey000@gmail.com>
2026-08-08 19:14:24 +0530

    Add proper authorization checks for protected routes so that only authenticated users can access them and redirect unauthenticated users to the sign-in page with a callback URL.

--- src/proxy.ts
+++ src/proxy.ts
@@ -5,32 +5,38 @@ import { getToken } from "next-auth/jwt"
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
-    const token = await getToken({req: request});
+    const token = await getToken({ req: request });
     const url = request.nextUrl;
 
     // Redirect to dashboard if the user is already authenticated/
     // signed-in and trying to access sign-in, sign-up, verify, or home page
-    if (token && (
-        url.pathname.startsWith("/sign-in") || 
-        url.pathname.startsWith("/sign-up") ||
-        url.pathname.startsWith("/verify")
-    )) {
+    if (token && (url.pathname === "/sign-in" || url.pathname === "/sign-up" || url.pathname.startsWith("/verify") )) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
 
-    if (!token && url.pathname.startsWith('/dashboard')) {
-        return NextResponse.redirect(new URL('/sign-in', request.url));
+    if (!token && ( url.pathname === "/dashboard" || url.pathname.startsWith("/u/") )) {
+        const signInUrl = new URL("/sign-in", request.url);
+
+        signInUrl.searchParams.set( 'callbackUrl', `${url.pathname}` );
+
+        return NextResponse.redirect(signInUrl);
+    }
+    else if (!token && url.pathname.startsWith("/verify")) {
+        const referer = request.headers.get("referer");
+
+        if (referer) {
+            const refererUrl = new URL(referer);
+            
+            if (refererUrl.pathname !== "/sign-up") {
+                return NextResponse.redirect(new URL("/sign-in", request.url));
+            }
+            else return NextResponse.next();
+        }
+
+        // if no referrer:
+        return NextResponse.redirect(new URL("/sign-in", request.url));
     }
 
-    return NextResponse.next();
-}
 
-export const config = {
-    matcher: [
-        "/",
-        "/sign-in",
-        "/sign-up",
-        "/dashboard/:path*",
-        "/verify/:path*"
-    ],
+    return NextResponse.next();
 }
