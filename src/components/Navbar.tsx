74ee4d92b57e52c3a5c3db980f183192bb5bfa32
VinayNoogler000 <vinaytambey000@gmail.com>
2026-08-08 15:27:14 +0530

    Add link to "/dashboard" page on the Navbar component, which appears on all pages except the "/dashboard" itself for seamless navigation and better user experience.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,39 @@
 "use client"
 import Link from 'next/link'
-import { useSession, signOut, signIn } from 'next-auth/react'
+import { useSession, signOut } from 'next-auth/react'
 import { User } from 'next-auth'
 import { Button } from './ui/button'
+import { usePathname } from 'next/navigation';
 
 function Navbar() {
-  const {data:session, status} = useSession();
+  const { data: session, status } = useSession();
   const user = session?.user as User;
+  const pathName = usePathname();
 
   return (
     <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
         <a href="#" className="text-xl font-bold mb-4 md:mb-0">AnonyMsg</a>
-        { status === "authenticated" ? 
-          ( 
+        {status === "authenticated" ?
+          (
             <>
-              <span className="mr-4">Welcome, {user?.username || user?.email}! </span> 
-              <Button className="w-fit md:w-auto bg-slate-100 text-black" onClick={() => signOut()}> Logout </Button> 
+              <span className="mr-4">Welcome, {user?.username || user?.email}! </span>
+
+              <div className="flex items-center gap-2 md:gap-5">
+                { pathName !== "/dashboard" &&
+                  <Button asChild className="w-fit md:w-auto bg-slate-100 text-black">
+                    <Link href="/dashboard">Dashboard</Link>
+                  </Button>
+                }
+
+                <Button className="w-fit md:w-auto bg-slate-100 text-black" onClick={() => signOut()}> Logout </Button>
+              </div>
             </>
-          ) : 
-          ( 
+          ) :
+          (
             <Link href={"/sign-in"}>
               <Button className="w-fit md:w-auto bg-slate-100 text-black" variant="outline">Login</Button>
-            </Link> 
+            </Link>
           )
         }
       </div>
